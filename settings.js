8ad8e04cc8711d35f2e7cef2597027f0c1c30687
clayouma69-creator <clayouma69@gmail.com>
2025-12-09 21:05:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎Knight Bot',
   botName: "Knight Bot",
-  botOwner:', /techlee/
+  botOwner:', /Knight Bot/
   ownerNumber: '254701663762', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
